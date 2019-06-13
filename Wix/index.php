390afabd7e0d4ca1c8c13c5a715ff2d0cd479b27
shresthanabin1 <51776884+shresthanabin1@users.noreply.github.com>
2019-06-13 13:58:59 +0545

    Add files via upload

---
+++ Wix/index.php
@@ -0,0 +1,347 @@
+<?php require "header.php"; ?>
+
+<section id="carouselslides" class="carousel slide carousel-fade shadow" data-ride="carousel">
+	<ul class="carousel-indicators">
+		<li class="active" data-target="#carouselslides" data-slide-to="0"></li>
+		<li data-target="#carouselslides" data-slide-to="1"></li>
+		<li data-target="#carouselslides" data-slide-to="2"></li>
+	</ul>
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img src="image/3.jpg" alt="#" class="img-fluid">
+		</div>
+		<div class="carousel-item">
+			<img src="image/second.jpg" alt="#" class="img-fluid">
+		</div>
+		<div class="carousel-item">
+			<img src="image/5.jpg" alt="#" class="img-fluid">
+		</div>
+	</div>
+</section>
+<section class="writer">
+	<div class="container text-lg-left text-md-right">
+		<div class="row">
+			<div class="col-md-4 mb-3">
+				<div class="pager position-relative pl-5 pr-3">
+					<a href="#" class="icon-part">
+						<img src="image/house.png" alt="rent" class="img-fluid">
+					</a>
+					<h6 class="text-uppercase">Hassle Free</h6>
+					<p class="text-secondary">Canvas provides support for Native HTML5 Videos that can be added to a Full Width Background.</p>
+				</div>
+			</div>
+
+			<div class="col-md-4 mb-3">
+				<div class="pager position-relative pl-5 pr-3">
+					<a href="#" class="icon-part">
+						<img src="image/hammer.png" alt="rent" class="img-fluid">
+					</a>
+					<h6 class="text-uppercase">Well Constructed</h6>
+					<p class="text-secondary">Display your Content attractively using Parallax Sections that have unlimited customizable areas.</p>
+				</div>
+			</div>
+
+			<div class="col-md-4 mb-3">
+				<div class="pager position-relative pl-5 pr-3">
+					<a href="#" class="icon-part">
+						<img src="image/construction.png" alt="rent" class="img-fluid">
+					</a>
+					<h6 class="text-uppercase">Free Utilities</h6>
+					<p class="text-secondary">You have complete easy control on each & every element that provides endless customization possibilities.</p>
+				</div>
+			</div>
+
+			<div class="col-md-4 py-3">
+				<div class="pager position-relative pl-5 pr-3">
+					<a href="#" class="icon-part">
+						<img src="image/rent.png" alt="rent" class="img-fluid">
+					</a>
+					<h6 class="text-uppercase">Flexible Rentals</h6>
+					<p class="text-secondary">Canvas provides support for Native HTML5 Videos that can be added to a Full Width Background.</p>
+				</div>
+			</div>
+
+			<div class="col-md-4 py-3">
+				<div class="pager position-relative pl-5 pr-3">
+					<a href="#" class="icon-part">
+						<img src="image/suitcase.png" alt="rent" class="img-fluid">
+					</a>
+					<h6 class="text-uppercase">Easy Financing</h6>
+					<p class="text-secondary">Display your Content attractively using Parallax Sections that have unlimited customizable areas.</p>
+				</div>
+			</div>
+
+			<div class="col-md-4 py-3">
+				<div class="pager position-relative pl-5 pr-3">
+					<a href="#" class="icon-part">
+						<img src="image/contract.png" alt="rent" class="img-fluid">
+					</a>
+					<h6 class="text-uppercase">Solid Paperwork</h6>
+					<p class="text-secondary">You have complete easy control on each & every element that provides endless customization possibilities.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="properties">
+	<div class="container ">
+		<div class="row">
+			<div class="col-md-6 col-md-center">
+				<h3 class="text-uppercase">Featured Properties</h3>
+			</div>
+			<div class="col-md-6 text-lg-right text-md-center">
+				<button class="btn btn-light text-uppercase text-center check">Check All</button>
+			</div>
+		</div>
+	</div>
+	<div class="container py-5">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="top-part"></div>
+				<div class="main d-flex">
+					<div class="first-one">
+						<h4>3 Bedroom Villa</h4>
+						<p class="text-secondary">Seminyak Area</p>
+					</div>
+					<!-- <div class="second-one">
+						<a href="#" class="exclamation"><i class="fas fa-exclamation"></i></a>
+						
+					</div> -->
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-4 nopadding">
+						Beds:
+						<span>3</span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Baths:
+						<span>3</span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Area:
+						<span>150 sqm</span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Pool:
+						<span><i class="fas fa-check-square corrects"></i></span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Internet:
+						<span><i class="fas fa-check-square corrects"></i></span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Cleaning:
+						<span><i class="fas fa-minus-square minus"></i></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="middle-part"></div>
+				<div class="main d-flex">
+					<div class="first-one">
+						<h4>3 Bedroom Villa</h4>
+						<p class="text-secondary">Seminyak Area</p>
+					</div>
+					<!-- <div class="second-one">
+						<a href="#" class="exclamation"><i class="fas fa-exclamation"></i></a>
+						
+					</div> -->
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-4 nopadding">
+						Beds:
+						<span>3</span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Baths:
+						<span>3</span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Area:
+						<span>150 sqm</span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Pool:
+						<span><i class="fas fa-check-square corrects"></i></span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Internet:
+						<span><i class="fas fa-check-square corrects"></i></span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Cleaning:
+						<span><i class="fas fa-minus-square minus"></i></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="bottom-part"></div>
+				<div class="main d-flex">
+					<div class="first-one">
+						<h4>3 Bedroom Villa</h4>
+						<p class="text-secondary">Seminyak Area</p>
+					</div>
+					<!-- <div class="second-one">
+						<a href="#" class="exclamation"><i class="fas fa-exclamation"></i></a>
+						
+					</div> -->
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-4 nopadding">
+						Beds:
+						<span>3</span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Baths:
+						<span>3</span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Area:
+						<span>150 sqm</span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Pool:
+						<span><i class="fas fa-check-square corrects"></i></span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Internet:
+						<span><i class="fas fa-check-square corrects"></i></span>
+					</div>
+					<div class="col-md-4 nopadding">
+						Cleaning:
+						<span><i class="fas fa-minus-square minus"></i></span>
+					</div>
+				</div>				
+			</div>
+		</div>
+	</div>
+
+<div class="container p-4 contact-us">
+	<div class="row">
+		<div class="col-md-9">
+			<h4 class="text-white">Special Offers on Villa Long Term Rentals & Lease Aggrements</h4>
+		</div>
+		<div class="col-md-3 text-right">
+			<button class="btn btn-warning text-center text-uppercase now">Contact Now</button>
+		</div>
+	</div>
+</div>
+
+<div class="container pb-5">
+	<div class="row">
+		<div class="col-lg-7">
+			<a href="#" class="background"></a>
+		</div>
+		<div class="col-lg-5">
+			<a href="#" class="background1"></a>
+		</div>
+		<div class="col-lg-4">
+			<a href="#" class="background2"></a>
+		</div>
+		<div class="col-lg-4">
+			<a href="#" class="background3"></a>
+		</div>
+		<div class="col-lg-4">
+			<a href="#" class="background4"></a>
+		</div>
+	</div>
+</div>
+</section>
+
+<section class="form" id="about">
+	<div class="container-fluid info text-center ">
+		<div class="row">
+			<div class="col-lg-5"></div>
+			<div class="col-lg-3 text-lg-left">
+				<h5><b>Our Headquarters</b></h5><br>
+				<p class="m-0 text-secondary"><b>North America:</b></p>
+				<p class="text-secondary">795 Folsom Ave, Suite 600 <br>
+				San Francisco, CA 94107.</p>
+				<p class="m-0 text-secondary"><b>Phone: </b>(91) 8547 632521</p>
+				<p class="text-secondary"><b>Email: </b>real-estate@canvas.com</p>
+				<h5 class="text-secondary"><b>Working Hours:</b></h5>
+				<p class="m-0 text-secondary"><b>Mon-Fri: </b>10AM to 6PM</p>
+				<p class="m-0 text-secondary"><b>Saturday: </b> to 3PM</p>
+				<p class="m-0 text-secondary"><b>Sunday: </b>Closed</p>
+
+			</div>
+			<div class="col-lg-4 info-part">
+				<h4 class="text-white pb-4">Get A Quick Quote</h4>
+				<div class="forms">
+					<input type="text" name="fullname" value="" placeholder="Full Name"><br>
+					<input type="email" name="email" value="" placeholder="Email Address"><br>
+					<input type="text" name="phone" value="" placeholder="Phone Number (+1-555-2221)"><br>
+					<textarea type="text" name="contents" placeholder="What are you Looking for? (Ex: Villa on the Beach)" rows="5" cols="40"></textarea><br>
+					<button class="btn btn-light text-uppercase">Send Email</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- <section class="upoer-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-7">
+				<ul class="nav nav-tabs" id="myTab" role="tablist">
+					<li class="nav-item">
+						<a href="#why" class="nav-link active" id="why-tab" data-toggle="tab" rolo="tab" aria-controls="why" aria-selected="true">Why Us?</a>
+					</li>
+					<li class="nav-item">
+						<a href="#properties" class="nav-link" id="properties-tab" data-toggle="tab" rolo="tab" aria-controls="properties" aria-selected="true">Properties</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link" id="legal-tab" data-toggle="tab" rolo="tab" aria-controls="legal" aria-selected="true">Legal</a>
+					</li>
+				</ul>
+				<div class="tab-content" id="mytabContent">
+					<div class="tab-pane fade show active" id="why" role="tabpanel" aria-labelledby="why-tab">
+						<ul class="treklist list-unstyled">
+                         <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+                        </ul>
+					</div>
+
+					<div class="tab-pane fade show active" id="properties" role="tabpanel" aria-labelledby="properties-tab">
+						<ul class="treklist list-unstyled">
+								<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
+
+                                <li><a href="https://www.wadventuresnepal.com/tso-rolpa-lake-trek/">
+                                                 </a>
+                                </li>
+                                <li><a href="https://www.wadventuresnepal.com/upper-mustang-trek/">
+                                    Upper Mustang Trek              </a>
+                                </li>
+                                <li><a href="https://www.wadventuresnepal.com/langtang-valley-trek/">
+                                    Langtang Valley Trek              </a>
+                                </li>
+                                <li><a href="https://www.wadventuresnepal.com/rolwalling-tashi-laptsa-pass-trek/">
+                                    Rolwalling Tashi Laptsa Pass Trek              </a>
+                                </li>
+                                <li><a href="https://www.wadventuresnepal.com/langtang-gosaikunda-dhunche-trek/">
+                                    Langtang Gosaikunda Dhunche Trek              </a>
+                                </li>
+                                <li><a href="https://www.wadventuresnepal.com/gokyo-trek-via-renjo-la-pass/">
+                                    Gokyo Trek via Renjo La Pass              </a>
+                                </li>
+                        </ul>
+					</div>
+
+					<div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+					<div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+				</div>
+							</div>
+			<div class="col-lg-5"></div>
+		</div>
+	</div>
+</section>
+ -->
+
+ 
+
+<?php require "footer.php"; ?>
